11ad0a7d5d317fee4786c1c7a327af440514cb85
FOAMLabs <web3welfarequeen@proton.me>
2024-02-16 22:40:06 -0800

    chore:(cleaned up code base)

--- src/libraries/DeploymentTracker.sol
+++ src/libraries/DeploymentTracker.sol
@@ -2,24 +2,17 @@
 pragma solidity ^0.8.23;
 
 contract DeploymentTracker {
-    // Event to be emitted upon new contract/library deployment registration
     event NewDeploymentRegistered(address indexed contractAddress, address indexed metadataLibrary);
 
-    // Mapping from contract address (or unique identifier) to its metadata library address
     mapping(address => address) public metadataLibraries;
 
-    // Function to register a new contract and its metadata library
     function registerContract(address contractAddress, address metadataLibrary) external {
         require(metadataLibraries[contractAddress] == address(0), "Contract already registered");
         
-        // Register the metadata library for the contract
         metadataLibraries[contractAddress] = metadataLibrary;
         
-        // Emit an event with the contract and metadata library addresses
         emit NewDeploymentRegistered(contractAddress, metadataLibrary);
     }
-
-    // Function to retrieve the address of a contract's metadata library
     function getMetadataLibrary(address contractAddress) external view returns (address) {
         require(metadataLibraries[contractAddress] != address(0), "Contract not registered");
         return metadataLibraries[contractAddress];
